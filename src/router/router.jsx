2565c0063c923566a052ceafd26d078793997bb2
aumartinez <acm_asd@hotmail.com>
2023-03-07 23:56:28 -0600

    Added not found page

--- src/router/router.jsx
+++ src/router/router.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import Root from '../pages/Root'
+import NotFound from '../pages/NotFound'
 import { createBrowserRouter } from 'react-router-dom'
 
 const router = createBrowserRouter([
   {
     path: '/',
     element: <Root />,
+    errorElement: <NotFound />
   }
 ])
 
